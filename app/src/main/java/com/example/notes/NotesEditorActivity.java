0d09c22c806f7cabab26c5f278afb530381cfe95
Purvesh Shende <56558717+purveshshende2@users.noreply.github.com>
2020-08-03 00:13:11 +0530

    adding new feature

--- app/src/main/java/com/example/notes/NotesEditorActivity.java
+++ app/src/main/java/com/example/notes/NotesEditorActivity.java
@@ -2,13 +2,48 @@ package com.example.notes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
 
 public class NotesEditorActivity extends AppCompatActivity {
 
+    int noteId;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes_editor);
+
+        EditText editText = findViewById(R.id.editText);
+        Intent intent = getIntent();
+        noteId = intent.getIntExtra("noteId",-1);
+
+        if(noteId != -1){
+            editText.setText(MainActivity.notes.get(noteId));
+        } else {
+            MainActivity.notes.add("");
+            noteId = MainActivity.notes.size() -1 ;
+        }
+
+        editText.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                MainActivity.notes.set(noteId,String.valueOf(s));
+                MainActivity.arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
     }
 }
